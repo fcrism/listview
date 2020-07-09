a1ee52469afd75b7a41b9aabb845b162c3148c86
Cristiano <cfmoreira7@hotmail.com>
2020-07-09 18:12:25 -0300

    Add ad contact and search listener

--- app/src/main/java/com/example/contact/ViewContactFragment.java
+++ app/src/main/java/com/example/contact/ViewContactFragment.java
@@ -5,10 +5,13 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class ViewContactFragment extends Fragment {
     private static final String TAG = "ViewContactsFragments";
@@ -17,6 +20,25 @@ public class ViewContactFragment extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         Log.d(TAG, "onCreateView: view started");
+
+        // Navegar ate add contacts
+        FloatingActionButton fab = (FloatingActionButton) getView().findViewById(R.id.btn_add);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.d(TAG, "onClick: clicked fab.");
+            }
+        });
+
+        ImageView ivSearchContact = (ImageView) getView().findViewById((R.id.ivSearchIcon));
+        ivSearchContact.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.d(TAG, "onClick: clicked search icon.");
+            }
+        });
+
         return inflater.inflate(R.layout.fragment_viewcontacts, container,false);
+
     }
 }
