39c2be527f398859b6fdafd185b8868701b224c8
Cristiano <cfmoreira7@hotmail.com>
2020-07-09 16:17:20 -0300

    Add main fragment function

--- app/src/main/java/com/example/contact/MainActivity.java
+++ app/src/main/java/com/example/contact/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.contact;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
 
@@ -10,5 +12,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+    private void init(){
+
+        //primeiro fragment
+        ViewContactFragment fragment = new ViewContactFragment();
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        //Substitui o que esta no container fragment com o fragment atribuido
+        transaction.replace(R.id.fragment_container, fragment);
+        transaction.addToBackStack(null); //retorna para o stack quando utiliza voltar
+        transaction.commit();
     }
 }
