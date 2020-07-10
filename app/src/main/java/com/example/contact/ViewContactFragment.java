6a37a5a1702887c8127455fe32be79ced37208c1
Cristiano <cfmoreira7@hotmail.com>
2020-07-09 22:26:12 -0300

    Add search appbar toggle state

--- app/src/main/java/com/example/contact/ViewContactFragment.java
+++ app/src/main/java/com/example/contact/ViewContactFragment.java
@@ -11,16 +11,27 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import com.google.android.material.appbar.AppBarLayout;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class ViewContactFragment extends Fragment {
     private static final String TAG = "ViewContactsFragments";
 
+    private static final int STANDARD_APPBAR = 0;
+    private static final int SEARCH_APPBAR = 1;
+    private int mAppBarState;
+
+    private AppBarLayout viewContactsBar, searchBar;
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         Log.d(TAG, "onCreateView: view started");
+
+        //Inicialização das views
         View view = inflater.inflate(R.layout.fragment_viewcontacts, container,false);
+        viewContactsBar = (AppBarLayout) view.findViewById(R.id.viewContactsToolbar);
+        searchBar = (AppBarLayout) view.findViewById(R.id.searchToolbar);
 
         // Navegar ate add contacts
         FloatingActionButton fab = (FloatingActionButton) view.findViewById(R.id.btn_add);
@@ -36,10 +47,44 @@ public class ViewContactFragment extends Fragment {
             @Override
             public void onClick(View view) {
                 Log.d(TAG, "onClick: clicked search icon.");
+                toggleToolBarState();
             }
         });
 
+        ImageView ivBackArrow = (ImageView) view.findViewById(R.id.ivBackArrow);
+        ivBackArrow.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.d(TAG, "onClick: clicked back arrow.");
+                toggleToolBarState();
+            }
+        });
         return view;
 
+    }
+    // alterna estados da barra search
+    private void toggleToolBarState() {
+        Log.d(TAG, "toggleToolBarState: toggling appbar state");
+        if(mAppBarState) == STANDARD_APPBAR){
+            setAPPBarState(SEARCH_APPBAR);
+        } else {
+            setAPPBarState(STANDARD_APPBAR);
+        }
+    }
+
+    private void setAPPBarState(int state) {
+        
+        Log.d(TAG, "setAPPBarState: Changing app bar state to: " + state);
+
+        mAppBarState = state;
+
+        if(mAppBarState == STANDARD_APPBAR){
+            searchBar.setVisibility(View.GONE);
+            viewContactsBar.setVisibility(View.VISIBLE);
+        }
+        else if(mAppBarState == SEARCH_APPBAR){
+            viewContactsBar.setVisibility(View.GONE);
+            searchBar.setVisibility(View.VISIBLE);
+        }
     }
 }
